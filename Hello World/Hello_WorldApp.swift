2dab0516aeeeb2a5a7bc646804809e4b8a36f5d3
Muhammad Nabeel <nabeelshakeel966@gmail.com>
2024-07-30 11:54:42 +0500

    Initial Commit

---
+++ Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by apple on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
